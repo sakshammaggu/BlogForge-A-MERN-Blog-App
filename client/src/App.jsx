0da9db8555575094346c7253bfd9217cde7cfa49
sakshammaggu <sakshammaggu80@gmail.com>
2024-05-16 21:32:48 +0530

    created the header and navbar section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
@@ -13,6 +14,7 @@ import Projects from './pages/Projects';
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
